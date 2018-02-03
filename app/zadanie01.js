49903032b5f39ace01d7137cfaa9039c795f49cf
Łukasz Dadlez <lukaszdadlez@gmail.com>
2018-02-04 00:18:16 +0100

    rozwiązanie zadania01. TODO: nie działa zapisywanie cookie w zadaniu02

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,18 @@
 //Twój kod
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static(path.join(__dirname, './public/zadanie01/')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/form', (req, res) => {
+  const { num1, num2 } = req.body;
+  const success = `<h1>BINGO! ${num2} jest dzielnikiem ${num1}</h1>`
+  const failure = `<h1>${num2} nie jest dzielnikiem ${num1}</h1>`
+
+  res.send(num1 % num2 !== 0 ? failure : success); // rozumiem następuje kastowanie na Number ze względu na działanie %?
+});
+
+app.listen(3000, () => console.log('serwer stoi na porcie 3000'));
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,27 @@
 //Twój kod
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const app = express();
+
+app.use(express.static(path.join(__dirname, './public/zadanie02/')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.post('/cookie/set', (req, res) => {
+  const { name, surname } = req.body;
+  
+  res.cookie('names', `${name},${surname}`);
+  res.send('ustawione');
+  console.log('Ciastko ustawione', `${name},${surname}`);
+});
+
+app.get('/cookie/show', (req, res) => {
+  console.log(req.cookie);
+  // const [ name, surname ] = req.cookies.names.split(',');
+  // console.log('names', `${name},${surname}`);
+});
+
+
+app.listen(3000, () => console.log('serwer stoi na porcie 3000'));
