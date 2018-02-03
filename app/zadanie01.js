a37f7f1218567f0f1bb4ec06c36f75044b774508
Greg Herbowicz <herbowicz@users.noreply.github.com>
2018-02-03 11:56:34 +0100

    Update zadanie01.js

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,17 @@
-//Twój kod
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({
+    extended: true  //body-parser deprecated undefined extended: provide extended option
+}));
+app.use(express.static('./public/zadanie01/'));
+
+app.post('/result', (req, res) => {
+    const {a, b} = req.body; 
+    let is = parseInt(a) % parseInt(b) === 0 ? '' : ' nie';
+    res.send('Liczba ' + b + is + ' jest dzielnikiem liczby ' + a);
+});
+
+app.listen(3000, () => {
+  console.log('Serwer uruchomiony na porcie 3000');
+});
