3553b669e2ab98a7bbb01ee275e8a2835f4cfdd1
Łukasz Dadlez <lukaszdadlez@gmail.com>
2018-02-04 23:00:35 +0100

    rozwiązania

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -4,6 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
+const serializeResponse = (value) => `<h1 style='text-align: center'>${value}</h1>`;
 
 app.use(express.static(path.join(__dirname, './public/zadanie02/')));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,15 +14,16 @@ app.post('/cookie/set', (req, res) => {
   const { name, surname } = req.body;
   
   res.cookie('names', `${name},${surname}`);
-  res.send('ustawione');
-  console.log('Ciastko ustawione', `${name},${surname}`);
+  res.send(serializeResponse(`Ciastko ustawione: "${name},${surname}"`));
 });
 
 app.get('/cookie/show', (req, res) => {
-  console.log(req.cookie);
-  // const [ name, surname ] = req.cookies.names.split(',');
-  // console.log('names', `${name},${surname}`);
+  const [ name, surname ] = req.cookies.names.split(',');
+  res.send(serializeResponse(`User name is: ${name} and surname is: ${surname}`));
 });
 
+app.get('/cookie/check', (req, res) => {
+  res.send(req.cookies ? serializeResponse('Bon a\'petit! There is a cookie here :)') : serializeResponse('Echo... echo... Nothing here.'));
+});
 
 app.listen(3000, () => console.log('serwer stoi na porcie 3000'));
