b55c903f4fcb65c35f6ed817bfda05c2d3e527c5
Gladkowski <marcingladkowski@gmail.com>
2018-02-05 23:59:10 +0100

    exer.1,2

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,38 @@
-//Twój kod
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+
+app.use(express.static('./public/zadanie02/'))
+
+app.post('/cookie/set', (req, res) => {
+    let name  = req.body.name;
+
+    res.cookie('name', name, {
+        maxAge : 31536000000,
+    });
+
+    res.send('Zapisano imie');
+    
+});
+
+app.get('/cookie/show', (req, res) => {
+    res.send('Ciastko ma wartość: ' + req.cookies.name); 
+});
+
+app.get('/cookie/check', (req, res) => {
+    let name = req.cookies.name;
+
+    if(name != 'undefined'){
+        res.send('Ciastko ma wartość: ' + name); 
+    } else {
+        res.send('Ciastko nie zostalo ustawione'); 
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
