2b2d69e4e431b506c54869b1363abc28c0e69a01
Łukasz Bułka <mckornik@poczta.fm>
2018-02-06 19:44:07 +0000

    Day6

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,22 @@
-//Twój kod
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded());
+
+
+app.use(express.static('./public/zadanie01/'));
+
+app.post('/sum', (req, res) => {
+ const {a, b} = req.body;
+
+ if (!(parseInt(a) % parseInt(b))){
+ 	res.send('Liczba B jest dzielnikiem liczby A.')
+ } else {
+ 	res.send('Liczba B nie jest dzielnikiem liczby A.');
+ }
+});
+
+
+app.listen(3000, () => {
+	console.log('Wystartowalem serwerek na porcie 3000');
+});
