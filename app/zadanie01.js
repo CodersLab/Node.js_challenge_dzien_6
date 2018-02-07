709dade7ce7aa509808c476a598e4e8a8d2296dc
Markowiecky <markowiecki.z@gmail.com>
2018-02-07 20:46:25 +0100

    Uzupełnione zadania - dzień 6

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,20 @@
-//Twój kod
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('./public/zadanie01'));
+
+app.post('/result', (req, res) => {
+  const {firstNumb,secondNumb} = req.body;
+  let result = '';
+  if(parseInt(firstNumb) % parseInt(secondNumb)){
+    result = `${firstNumb} jest dzielnikiem ${secondNumb}`;
+  }else{
+    result = `${firstNumb} nie jest dzielnikiem ${secondNumb}`;
+  }
+    res.send(result);
+});
+
+app.listen(3000,()=>{
+  console.log('Serwer uruchomiony na porcie 3000');
+});
