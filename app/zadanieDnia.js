1f2d5d079a548c9f401660e958794cd0743bdc7a
Your Name <you@example.com>
2019-02-26 16:05:28 +0100

    ZadanieDnia WiP

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,15 @@
 //Twój kod
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+app.use(express.static('./public/zadanieDnia'))
+// app.use('/public', express.static(__dirname + '/zadanieDnia'))
+
+app.use(bodyParser.urlencoded())
+app.use(cookieParser())
+
+app.listen(3000, () => {
+    console.log('dziala na 3000')
+})
